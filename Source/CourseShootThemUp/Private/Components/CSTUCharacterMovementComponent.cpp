209605a99f233abc44bbf2a570d1d1e0890d9bef
Dybbor <19dobrohotov99@mail.ru>
2022-11-18 00:37:35 +0300

    add gun pitch animation

--- Source/CourseShootThemUp/Private/Components/CSTUCharacterMovementComponent.cpp
+++ Source/CourseShootThemUp/Private/Components/CSTUCharacterMovementComponent.cpp
@@ -1,6 +1,5 @@
 // CourseShootThemUp
 
-
 #include "Components/CSTUCharacterMovementComponent.h"
 #include "Player/CSTUBaseCharacter.h"
 
@@ -8,5 +7,5 @@ float UCSTUCharacterMovementComponent::GetMaxSpeed() const {
 
     const float MaxSpeed = Super::GetMaxSpeed();
     const ACSTUBaseCharacter* Player = Cast<ACSTUBaseCharacter>(GetPawnOwner());
-    return Player && Player->IsRunning()  ? MaxSpeed* RunModifier: MaxSpeed;
+    return Player && Player->IsRunning() ? MaxSpeed * RunModifier : MaxSpeed;
 }
